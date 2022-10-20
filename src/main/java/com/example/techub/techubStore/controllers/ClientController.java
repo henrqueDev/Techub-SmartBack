624d95e9c2855fbc709a82c7e3112cd493b78fd6
Luís Henrique <luis.freire@assert.ifpb.edu.br>
2022-10-20 16:52:41 -0300

    Implementar update

--- src/main/java/com/example/techub/techubStore/controllers/ClientController.java
+++ src/main/java/com/example/techub/techubStore/controllers/ClientController.java
@@ -7,8 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,28 +31,55 @@ public class ClientController {
 	public ClientController(ClientRepository clientRepository) {
 		this.clientRepository = clientRepository;
 	}
-
-
-	@RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
-	@ResponseBody
-	public String helloClient( @PathVariable("nome") String clientName) {
-		return String.format("Hello %s", clientName);
-	}
-	
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity getClientById( @PathVariable Integer id){
+	public ResponseEntity<Client> getClientById( @PathVariable Integer id){
 		Optional<Client> clientFound = clientRepository.findById(id);
 		
 		if(clientFound.isPresent()) {
-			HttpHeaders headers = new HttpHeaders();
+			//HttpHeaders headers = new HttpHeaders();
 			ResponseEntity<Client> response = new ResponseEntity<>(clientFound.get(), HttpStatus.OK);
 			return ResponseEntity.ok(clientFound.get());
 		}
 		
 		return ResponseEntity.notFound().build();
 	}
+	
+	@PostMapping
+	@ResponseBody
+	public ResponseEntity<Client> save( @RequestBody() Client user) {
+		Client clientToSave = clientRepository.save(user);
+		return ResponseEntity.ok(clientToSave);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	@ResponseBody
+	public ResponseEntity<Client> delete( @PathVariable Integer id){
+		Optional<Client> user = clientRepository.findById(id);
+		
+		if(user.isPresent()) {
+			clientRepository.delete( user.get() );
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();
+		
+	}
+	
+	@PutMapping(value="/{id}")
+	@ResponseBody
+	public ResponseEntity<Object> update(@PathVariable Integer id, @RequestBody Client user){
+		return clientRepository
+				.findById(id)
+				.map( clientExist -> {
+					user.setId(clientExist.getId());
+					clientRepository.save(user);
+					return ResponseEntity.noContent().build();
+		}).orElseGet( () -> ResponseEntity.notFound().build());
+		
+	}
+	
 	
 	
 }
